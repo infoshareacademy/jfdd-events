699c2e5132123ad7b01fc0f4a02a8e3b1735fdbe
Kristo <k.olesinski@wp.pl>
2016-01-06 18:17:24 +0100

    narrowing menu

--- SCRIPTS/menuHeight.js
+++ SCRIPTS/menuHeight.js
@@ -1,4 +1,16 @@
 /**
  * Created by kristo on 05.01.16.
  */
-$('section#topNavBar').scrollHeight
+$(document).ready(function(){
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 200) {
+            $("#topNavBar").animate({height: "50px"},"slow");
+            $("#logoInNavBar").css("padding-top", "5px");
+            $("div#socialIconsInNavBar").css("margin-top", "-15px");
+            $("li").css("padding-top", "-5px");
+            }
+        else {
+            $("#topNavBar").animate({height: "70px"},"slow");
+        }
+    });
+});
