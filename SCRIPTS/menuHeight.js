3f03e1fe8f4c71560f85f7dc2a7afea4cd1fd989
k-olesinski <k.olesinski@wp.pl>
2016-01-08 09:30:41 +0100

    narrowing-wideningMenu

--- SCRIPTS/menuHeight.js
+++ SCRIPTS/menuHeight.js
@@ -3,13 +3,13 @@
  */
 $(document).ready(function(){
     $(window).scroll(function() {
-        if ($(document).scrollTop() > 300) {
+        if ($(document).scrollTop() > 400) {
             $(".topNavBar, .socialIconsInNavBar, ul.topNavBar").addClass("narrowingMenu");
             $(".topNavBar, .socialIconsInNavBar, ul.topNavBar").removeClass("wideningMenu");
             }
         else {
+            $(".topNavBar, .socialIconsInNavBar, ul.topNavBar").removeClass("narrowingMenu");
             $(".topNavBar, .socialIconsInNavBar, ul.topNavBar").addClass("wideningMenu");
-            $(".topNavBar, .socialIconsInNavBar, ul.topNavBar").removeClass("narrowMenu");
         }
     });
 });
