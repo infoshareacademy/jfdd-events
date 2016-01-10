d52184e3c54952e0b738f0873a0a2c2d027e0db7
Jakub Klos <jakub.klos91@gmail.com>
2016-01-10 13:53:58 +0100

    napisanie parallax'y od podstaw - skrocenie kodu o 97%

--- SCRIPTS/parallax.js
+++ SCRIPTS/parallax.js
@@ -4,63 +4,12 @@
 
 $(document).ready(function(){
 
-    (function( $ ){
-        var $window = $(window);
-        var windowHeight = $window.height();
 
-        $window.resize(function () {
-            windowHeight = $window.height();
-        });
+    document.getElementById('intro');
 
-        $.fn.parallax = function(xpos, speedFactor, outerHeight) {
-            var $this = $(this);
-            var getHeight;
-            var firstTop;
-            var paddingTop = 0;
+    $(window).scroll(function () {
+        $(window).scrollTop();
+    });
 
 
-            $this.each(function(){
-                firstTop = $this.offset().top;
-            });
-
-            if (outerHeight) {
-                getHeight = function(jqo) {
-                    return jqo.outerHeight(true);
-                };
-            } else {
-                getHeight = function(jqo) {
-                    return jqo.height();
-                };
-            }
-
-
-            if (arguments.length < 1 || xpos === null) xpos = "50%";
-            if (arguments.length < 2 || speedFactor === null) speedFactor = 0.1;
-            if (arguments.length < 3 || outerHeight === null) outerHeight = true;
-
-            function update(){
-                var pos = $window.scrollTop();
-
-                $this.each(function(){
-                    var $element = $(this);
-                    var top = $element.offset().top;
-                    var height = getHeight($element);
-
-                    // Check if totally above or totally below viewport
-                    if (top + height < pos || top > pos + windowHeight) {
-                        return;
-                    }
-
-                    $this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos) * speedFactor) + "px");
-                });
-            }
-
-            $window.bind('scroll', update).resize(update);
-            update();
-        };
-    })(jQuery);
-
-
-    $('#zajawka').parallax(0.1);
-    $('#intro').parallax("83%", 0.4);
 });
