04b9d9c3343d56dd88c4fc5f1866d1c519549337
MonikaKal <m.kaluzynska@wp.pl>
2016-01-13 19:50:52 +0100

    Icons in game (show/hide)

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -13,17 +13,34 @@ $(document).ready(function() {
         $('.game-intro').css("display", "none");
         $(".pop-up").show(1000);
         $(".gameClose").show();
+        var days=getDays();
+            for (i=0; i<days.length; i++){
+                $("#"+days[i]).attr("src","images/"+icons[i]);
+                $("#"+days[i]).show();
+            }
     });
+
     $(".gameClose").click(function () {
         $(".gameClose").hide();
-        $(".pop-up").hide(500, function () {
-            $(".gameBackground").fadeOut(500);
-        });
+        $(".pop-up").hide(500);
     });
-    $(".gameBackground").click(function () {
-        $(".gameClose").hide();
-        $(".pop-up").hide(500, function () {
-            $(".gameBackground").fadeOut(500);
-        });
+
+    /* handler for hiding icons in calendar  */
+    $(".gameIcon").click(function () {
+        $(this).hide();
     });
-});
+   });
+
+//get list of 10 random days
+function getDays(){
+   var days=[];
+   while (days.length < 10){
+    var day=(Math.floor(Math.random()*29)+1);
+    if (days.indexOf(day) == -1){
+        days.push(day);
+    }
+   }
+    return days
+}
+
+var icons=["child.png", "cinema.png", "concert.png", "sport.png", "theatre.png", "child.png", "cinema.png", "concert.png", "sport.png", "theatre.png"];
