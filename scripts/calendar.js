49a79ed8555eccb8dd6cef0a05c31ca37bfaf18f
Monika Kałużyńska <m.kaluzynska@wp.pl>
2016-01-13 13:34:08 +0100

    Merge branch 'master' of https://github.com/infoshareacademy/jfdd-events
    Conflicts:
    	CSS/stylesheet.css
    	index.html

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -2,10 +2,12 @@
  * Created by monika on 13.01.16.
  */
 $(document).ready(function() {
+
     $(".gameStart").click(function () {
         $(".gameBackground").fadeIn(500, function () {
             $(".pop-up").show(1000);
             $(".gameClose").show();
+
         });
     });
     $(".gameClose").click(function () {
@@ -21,3 +23,10 @@ $(document).ready(function() {
         });
     });
 });
+
+////losowanie liczby z zakresu 1-29
+//var x=1+Math.floor(Math.random()*15)
+//
+//for(x=1; x<=29; x++){
+//    console.log(x)
+//}
