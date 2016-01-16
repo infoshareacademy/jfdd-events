56f52c3a61b4d48ea475afb7d18519ef8507b63c
Jakub Klos <jakub.klos91@gmail.com>
2016-01-16 22:14:36 +0100

    form validation improvement

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -5,7 +5,7 @@ var checkbox = document.myForm;
 
 
 function validateForm() {
-    if(firstName.value == "") {
+    if(firstName.value == "" || !isNaN(firstName.value) ) {
         document.getElementById("validateName").innerHTML = "Uzupełnij swoje imię!";
         firstName.focus();
         return false;
@@ -13,7 +13,7 @@ function validateForm() {
             document.getElementById("validateName").innerHTML = "";
     }
 
-    if(phoneNumber.value.length < 9  ||  phoneNumber.value.length >= 14) {
+    if(phoneNumber.value.length < 9  ||  phoneNumber.value.length >= 14  || isNaN(phoneNumber.value)) {
         document.getElementById("validateNumber").innerHTML = "Podaj poprawny numer telefonu!";
         phoneNumber.focus();
         return false;
